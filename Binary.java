86487880f668f8286d1c22998d9fe0948aa64d21
Nelson Luziga <nelsonluziga@yahoo.com>
2021-11-03 20:34:45 +0300

    Program to print binary numbers

---
+++ Binary.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class Binary {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int i = 0;
+        int power = 1;
+        while (power <= n/2){
+            power = 2 * power;
+        }
+        while (power > 0){
+            if (n<power)
+                System.out.print(0);
+            else{
+                System.out.print(1);
+                n -= power;
+            }
+            power /= 2;
+        }
+        System.out.println();
+    }
+}
